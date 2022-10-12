ff3513663814a60e4fb7ccade97cf31fef1422e1
sbswag9@gmail.com <sbswag9@gmail.com>
2022-10-12 19:53:39 +0600

    added Topics screen

--- src/screens/TopicScreen.js
+++ src/screens/TopicScreen.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function TopicScreen() {
-  return <div>TopicScreen</div>;
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://openapi.programming-hero.com/api/quiz").then((data) => {
+      setTopics(data.data.data);
+    });
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-5 md:grid md:grid-cols-4">
+      {topics.map((item) => (
+        <div key={item.id} className="p-2 bg-white rounded-md max-w-xl ">
+          <img
+            src={item.logo}
+            alt=""
+            className="bg-purple-200 rounded-md mb-2"
+          />
+          <div className="flex justify-between items-center">
+            <h3 className="text-md font-bold text-purple-600">{item.name}</h3>
+            <Link to={`/topics/${item.id}`}>
+              <button className="bg-purple-400 p-1 text-white font-semibold rounded-md">
+                Start Practice
+              </button>
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default TopicScreen;
