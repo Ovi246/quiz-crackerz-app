f8efff16fbd175c21a1937f120c35a5769db7717
sbswag9@gmail.com <sbswag9@gmail.com>
2022-10-12 19:54:23 +0600

    added 404 not found page

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <h1 className="text-4xl text-center font-bold mt-[40%]">
+      404, Sorry This Page Doesn't Exist :(
+    </h1>
+  );
 }
 
 export default NotFound;
